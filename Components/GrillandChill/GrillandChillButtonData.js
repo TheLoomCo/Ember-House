fcaf8c596215c467a78cea31e802bd5bcbcec22f
Ladd1993 <aladd1993@gmail.com>
2022-07-06 23:00:47 -0500

    category functionality

---
+++ Components/GrillandChill/GrillandChillButtonData.js
@@ -0,0 +1,20 @@
+export const ButtonData = [
+    {
+        title: "hors d'oeuvres"
+    },
+    {
+        title: "entrees"
+    },
+    {
+        title: "desserts"
+    },
+    {
+        title: "cocktails"
+    },
+    {
+        title: "wine"
+    },
+    {
+        title: "clear filter"
+    }
+]
--- Components/GrillandChill/index.jsx
+++ Components/GrillandChill/index.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
+import { ButtonData } from './GrillandChillButtonData'
 
 const GrillandChill = () => {
+
+    const [category, setCategory] = useState('clear filter')
+
     return (
         <main className="ember-house-home-main">
             <div className='home-page-headers'>
@@ -9,35 +13,78 @@ const GrillandChill = () => {
                 <h4>Come and see what Ember House has to offer!</h4>
             </div>
             <div className='category-btns'>
-                <button>hors d'oeuvres</button>
-                <button>entrees</button>
-                <button>desserts</button>
-                <button>cocktails</button>
-                <button>wine</button>
+                {
+                    ButtonData.map((button) => (
+                        <button onClick={
+                            () => setCategory(button.title)
+                        }>{button.title}</button>
+                    ))
+                }
             </div>
 
             {/* Menu Preview */}
-            <div className='menu-preview-section'>
+            {
+                category === "hors d'oeuvres" && (
+                    <div className='menu-preview'>
+                        <h2>Hors D'oeuvres</h2>
+                        <img src="/placeholder-menu.png" alt="" />
+                    </div>
+                )
+            }
+            {
+                category === "entrees" && (
+                    <div className='menu-preview'>
+                        <h2>Entrees</h2>
+                        <img src="/placeholder-menu.png" alt="" />
+                    </div>
+                )
+            }
+            {
+                category === "desserts" && (
+                    <div className='menu-preview'>
+                        <h2>Desserts</h2>
+                        <img src="/placeholder-menu.png" alt="" />
+                    </div>
+                )
+            }
+            {
+                category === "cocktails" && (
+                    <div className='menu-preview'>
+                        <h2>Cocktails</h2>
+                        <img src="/placeholder-menu.png" alt="" />
+                    </div>
+                )
+            }
+            {
+                category === "wine" && (
+                    <div className='menu-preview'>
+                        <h2>Wine</h2>
+                        <img src="/placeholder-menu.png" alt="" />
+                    </div>
+                )
+            }
+            {
+                category === 'clear filter' && (
+                    <div className='menu-preview-section'>
 
-                <img src="/entrees.png" alt="entrees" />
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar neque neque, sit amet scelerisque enim mollis vel. Vestibulum sagittis lacus nec turpis consequat, vel pellentesque quam venenatis.
-                </p>
+                        <img src="/entrees.png" alt="entrees" />
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar neque neque, sit amet scelerisque enim mollis vel. Vestibulum sagittis lacus nec turpis consequat, vel pellentesque quam venenatis.
+                        </p>
 
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar neque neque, sit amet scelerisque enim mollis vel. Vestibulum sagittis lacus nec turpis consequat, vel pellentesque quam venenatis.
-                </p>
-                <img src="/cocktails.png" alt="cocktails" />
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar neque neque, sit amet scelerisque enim mollis vel. Vestibulum sagittis lacus nec turpis consequat, vel pellentesque quam venenatis.
+                        </p>
+                        <img src="/cocktails.png" alt="cocktails" />
 
-                <img src="/entertainment.png" alt="entertainment" />
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar neque neque, sit amet scelerisque enim mollis vel. Vestibulum sagittis lacus nec turpis consequat, vel pellentesque quam venenatis.
-                </p>
-
-
-
-            </div>
-        </main>
+                        <img src="/entertainment.png" alt="entertainment" />
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar neque neque, sit amet scelerisque enim mollis vel. Vestibulum sagittis lacus nec turpis consequat, vel pellentesque quam venenatis.
+                        </p>
+                    </div>
+                )
+            }
+        </main >
     )
 }
 
