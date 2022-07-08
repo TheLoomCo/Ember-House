a749fb10641112512fb55f284ef8b733ad1467aa
Ladd1993 <aladd1993@gmail.com>
2022-07-08 03:03:01 -0500

    Building Mobile Navigation and testing logo

--- Components/Navigation/index.jsx
+++ Components/Navigation/index.jsx
@@ -8,15 +8,26 @@ import { BsFacebook, BsInstagram, BsTelephoneFill, BsTwitter } from 'react-icons
 const Navigation = () => {
     const router = useRouter();
     const [clientWindowHeight, setClientWindowHeight] = useState("");
+    const [clientWindowWidth, setClientWindowWidth] = useState("")
+    const [toggleMobileMenu, setToggleMobileMenu] = useState(false)
 
     useEffect(() => {
         window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
+        window.addEventListener("resize", handleResize);
+        console.log(window.screen.availWidth)
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+            window.removeEventListener("resize", handleResize);
+        }
     });
 
     const handleScroll = () => {
         setClientWindowHeight(window.scrollY);
     };
+
+    const handleResize = () => {
+        setClientWindowWidth(window.screen.availWidth);
+    }
 
     return (
         <div className={clientWindowHeight > 50 ? 'navbar-style-wrapper background-fill' : 'navbar-style-wrapper'}>
@@ -26,11 +37,27 @@ const Navigation = () => {
                         <BsTelephoneFill />
                         <span>(123)-456-7890</span>
                     </div>
-                    <Link href="/">
-                        <a className="nav-logo">
-                            <img src={clientWindowHeight > 50 ? '/logo-black.png' : '/logo-white.png'} alt="Ember House" width="150" height="90" />
-                        </a>
-                    </Link>
+                    {
+                        clientWindowWidth
+                            ?
+                            <Link href="/">
+                                <a className="nav-logo">
+                                    <img className="nav-logo" src={clientWindowHeight > 50 ? '/logo-black.png' : '/logo-white.png'} alt="Ember House" width="150" height="80" />
+                                </a>
+                            </Link>
+                            :
+                            <Link href="/">
+                                <a className="nav-logo">
+                                    <img className="nav-logo" src='/logo-white.png' alt="Ember House" width="150" height="80" />
+                                </a>
+                            </Link>
+                    }
+                    <button id="mobileMenuTrigger" className={` ${toggleMobileMenu ? 'menuToggle' : ''}`} aria-expanded={toggleMobileMenu ? 'true' : 'false'} aria-controls="navigationLinks"
+                        title="Open and Close Navigation" onClick={() => setToggleMobileMenu(!toggleMobileMenu)}>
+                        <div className="line1"></div>
+                        <div className="line2"></div>
+                        <div className="line3"></div>
+                    </button>
                     <div className='nav-socials'>
                         <Link href="/"><a className={clientWindowHeight > 50 ? ' color-black' : ''} target="_blank" rel="noreferrer"><BsFacebook title="Visit Facebook" /></a></Link>
                         <Link href="/"><a className={clientWindowHeight > 50 ? ' color-black' : ''} target="_blank" rel="noreferrer"><BsInstagram title="Visit Instagram" /></a></Link>
@@ -38,12 +65,12 @@ const Navigation = () => {
                     </div>
                 </div>
 
-                <ul className="nav-list">
+                <ul className={toggleMobileMenu ? 'nav-list active-links' : 'nav-list'} id="navigationLinks">
                     {
                         NavLinkData.map((link) => (
                             <li key={link?.id} >
                                 <Link href={link?.url}>
-                                    <a className={`${router.pathname === link?.url ? 'active' : ''} ${clientWindowHeight > 50 ? 'color-black' : ''} `}>
+                                    <a onClick={() => setToggleMobileMenu(false)} className={`${router.pathname === link?.url ? 'active' : ''} ${clientWindowHeight > 50 ? 'color-black' : ''} `}>
                                         {link?.title}
                                     </a>
                                 </Link>
