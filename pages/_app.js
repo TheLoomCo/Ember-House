73fd740b516137a1a6f9f9d070a7addf1956be9a
Ladd1993 <aladd1993@gmail.com>
2022-07-11 12:06:21 -0500

    OG Image & Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -11,9 +11,36 @@ import '../styles/about-page.css'
 // About Component
 import '../Components/About/about.modules.css';
 import '../Components/About/TeamMember/team-member.modules.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* OG Image */}
+        <meta property="og:title" content="Ember House" />
+        <meta property="og:site_name" content="Ember House" />
+        <meta property="og:url" content="https://ember-house.vercel.app" />
+        <meta property="og:description" content="" />
+        <meta property="og:type" content="restaurant" />
+        <meta property="og:image" content="https://ember-house.vercel.app/logo-white.png" />
+
+        {/* Favicon */}
+
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#ffffff" />
+
+
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
